c9a25df56a471895fa7741a76d12693d1dbfad7a
eshan786 <eshan786behal@gmail.com>
2019-06-05 12:56:53 +0530

    learnt arithematics and string concatenation.

--- app/src/main/java/BehalTech/learnjava/HelloWorld.java
+++ app/src/main/java/BehalTech/learnjava/HelloWorld.java
@@ -5,13 +5,25 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Here's Eshan It's Just The Beginning");
 
-        String EB = "ESHAN BEHAL";
-        System.out.println(EB);
+        String eb =" ESHAN BEHAL";
+        System.out.println(eb);
 
-        int EBSalary = 45;
-        int MonthlySalary = EBSalary * 6;
-        System.out.println(EBSalary);
-        System.out.println(MonthlySalary);
+        int ebWeeklySalary = 45;
+        int ebMonthlySalary = ebWeeklySalary * 4;
+        System.out.println("Weekly Salary: " + ebWeeklySalary);
+        System.out.println("Monthly Salary: " + ebMonthlySalary);
+
+        int apples = 10;
+        int bananas = 6;
+        int fruits = apples + bananas;
+        System.out.println("I Have: " + fruits + " Fruits");
+
+        int weeks = 130;
+        double years = weeks / 52.0;
+        System.out.println(weeks + " Weeks is:" + years + " Years");
+
+        System.out.println("Hello" + eb + " Here Signing off.");
+
 
     }
 }
--- readme.md
+++ readme.md
@@ -9,3 +9,28 @@ KEEP CHECKING!!!!WILL BE UPDATED SOON..:)
     
     source code updated .
 
+2. Learned about arithematics and string concatenation
+
+   using variable types like int , double , string , float.
+
+   learnt something really amazing about variable type double
+
+   double - it is used most commonly for the fractional type calculations
+            float also does something like this but double here ss used
+            mostly.
+
+    and with help of string concatenation we can give more sense to our code and 
+    also can add numeric values to the code.
+    
+     source code updated. Can check if you want to see the use of both.
+
+
+
+
+
+
+
+
+
+     BY ESHAN_BEHAL  
+
