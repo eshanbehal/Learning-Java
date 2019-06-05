eedef9ca0f58609aaf9671b7d33a9f5afd4b9723
eshan786 <eshan786behal@gmail.com>
2019-06-05 15:31:20 +0530

    learnt about conditional processing and boolean type.

--- app/src/main/java/BehalTech/learnjava/HelloWorld.java
+++ app/src/main/java/BehalTech/learnjava/HelloWorld.java
@@ -2,28 +2,15 @@ package BehalTech.learnjava;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Here's Eshan It's Just The Beginning");
+        int lives = 3;
+        boolean isGameOver = (lives<1);
+        System.out.println(isGameOver);
 
-        String eb =" ESHAN BEHAL";
-        System.out.println(eb);
-
-        int ebWeeklySalary = 45;
-        int ebMonthlySalary = ebWeeklySalary * 4;
-        System.out.println("Weekly Salary: " + ebWeeklySalary);
-        System.out.println("Monthly Salary: " + ebMonthlySalary);
-
-        int apples = 10;
-        int bananas = 6;
-        int fruits = apples + bananas;
-        System.out.println("I Have: " + fruits + " Fruits");
-
-        int weeks = 130;
-        double years = weeks / 52.0;
-        System.out.println(weeks + " Weeks is:" + years + " Years");
-
-        System.out.println("Hello" + eb + " Here Signing off.");
-
-
+        if (isGameOver){
+            System.out.println("GameOver");
+        }
+        else{
+            System.out.println("You're Alive Here!!! ");
+        }
     }
 }
