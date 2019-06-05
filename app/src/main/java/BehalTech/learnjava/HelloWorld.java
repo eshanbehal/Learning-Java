dbccc520b7d17ffe1d1bae5ca41983cb8ba93ae6
eshan786 <eshan786behal@gmail.com>
2019-06-05 11:31:35 +0530

    Started learning java.Hello world

---
+++ app/src/main/java/BehalTech/learnjava/HelloWorld.java
@@ -0,0 +1,9 @@
+package BehalTech.learnjava;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Here's Eshan It's Just The Beginning");
+
+    }
+}
